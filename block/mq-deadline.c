fee5897b3f756d52abdc88d677b78e353e535b36
Hyobyung Han <1hanhb@naver.com>
2025-12-07 00:07:02 +0900

    fix bug
    fix wrong condition (@ dd dispatch)

--- block/mq-deadline.c
+++ block/mq-deadline.c
@@ -436,8 +436,6 @@ static struct request *__dd_dispatch_request(struct deadline_data *dd,
 		goto done;
 	}
 
-
-
 	/*
 	 * batches are currently reads XOR writes
 	 */
@@ -539,12 +537,12 @@ done:
 static struct request *dd_dispatch_request(struct blk_mq_hw_ctx *hctx)
 {
 	struct deadline_data *dd = hctx->queue->elevator->elevator_data;
-	struct request *rq;
+	struct request *rq = NULL;
 	enum dd_prio prio;
 
 	spin_lock(&dd->lock);
 
-	if (atomic_read(&dd->inflight))
+	if (atomic_read(&dd->inflight) == 0)
 	{
 		for (prio = 0; prio <= DD_PRIO_MAX; prio++) {
 			rq = __dd_dispatch_request(dd, &dd->per_prio[prio]);
